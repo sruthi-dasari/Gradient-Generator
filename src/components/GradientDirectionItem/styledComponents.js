11b9ff0254d80ed2958b5707a0cd548e4c5845b9
sruthi-dasari <113626139+sruthi-dasari@users.noreply.github.com>
2023-04-03 13:00:34 +0530

    Update styledComponents.js

--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components'
 
 export const Button = styled.button`
-  height: 40px;
-  width: 150px;
   color: #334155;
-  border-radius: 5px;
-  background-color: #ffffff79;
-  font-weight: 500px;
+  font-weight: 700px;
   font-family: 'Roboto';
-  //   border: none;
+  font-size: 20px;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: transparent;
 `
 
 export const ListItem = styled.li`
   list-style: none;
+  height: 55px;
+  width: 180px;
+  border-radius: 7px;
+//   background-color: ${props => (props.isActive ? '#ffffff' : '#ffffff79')};
+  background-color: #ffffff;
+  opacity: ${props => (props.isActive ? '1' : '0.5')}
+  margin: 0px;
 `
